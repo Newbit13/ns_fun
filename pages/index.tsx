e86b0dff064d6d0bcf46351a0b47cd103b7018da
Newbit13 <525842854@qq.com>
2021-01-25 13:42:31 +0800

    feat：引入serverless

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 import Cart from '../component/Card'
 import Layout from '../component/Layout'
 
-export default function Home() {
+export default function Home({gamelist}) {
   return (
     <Layout>
       <div className={styles.container}>
@@ -13,23 +13,28 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className={styles.main}>
-          <div className={styles.card_box}>
-            <Cart />
-          </div>
-          <div className={styles.card_box}>
-            <Cart />
-          </div>
-          <div className={styles.card_box}>
-            <Cart />
-          </div>
-          <div className={styles.card_box}>
-            <Cart />
-          </div>
-          <div className={styles.card_box}>
-            <Cart />
-          </div>
+          {
+            gamelist.map((game)=>{
+              return (
+                <div className={styles.card_box} key={game.id}>
+                  <Cart />
+                </div>
+              )
+            })
+          }
         </main>
       </div>
     </Layout>  
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://service-75oyrwi6-1256879764.gz.apigw.tencentcs.com/release/gamelist')
+  const gamelist = await res.json();
+  
+  return {
+    props: {
+      gamelist,
+    },
+  }
 }
